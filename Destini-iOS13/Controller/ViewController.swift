62748e132ccfdbce4a7b178eff0b06c90d1f6c64
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-16 18:31:47 +0100

    Added Story data to label and buttons

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var option1Button: UIButton!
+    @IBOutlet weak var option2Button: UIButton!
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    let storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
+    
+    func updateUI() {
+        let story = storyBrain.getCurrentStory()
+        storyLabel.text = story.title
+        option1Button.setTitle(story.choice1, for: .normal)
+        option2Button.setTitle(story.choice2, for: .normal)
+    }
 }
 
